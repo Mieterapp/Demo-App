5ec945ded02b4aa9d9b14dbe97b835e047b92498
Oliver Schrey <oliver.schrey@dit-digital.de>
2024-07-15 11:15:56 +0200

    cleanup

--- src/themes/dark.js
+++
@@ -1,9 +0,0 @@
-import { getColor } from './colors';
-
-const darkTheme = (contrast = 0, checkNumber) => {
-    return {
-
-    }
-}
-
-export default darkTheme;
--- src/themes/light.js
+++
@@ -1,9 +0,0 @@
-import { getColor } from './colors';
-
-const lightTheme = (contrast = 0, checkNumber) => {
-    return {
-
-    }
-}
-
-export default lightTheme;
